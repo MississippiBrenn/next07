7e85bc4d7d889cdbe15681bbeaf74f129e891778
Mississippi Brennan <mbrennan@blueorigin.com>
2023-05-31 09:30:34 -0700

    form returning 405

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,7 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+import { NextResponse } from 'next/server';
+
+//statically evaluated, no variation in request or info we depend on from incoming request
+//back end api endpoint
+export async function GET() {
+  return NextResponse.json({ message: 'Hello, Next.js!' });
 }
